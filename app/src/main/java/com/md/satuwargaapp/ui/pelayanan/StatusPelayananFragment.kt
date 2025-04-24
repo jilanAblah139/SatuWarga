34c08f76f3c62efeb09e76902c5349b4222a250e
Aqilah Qathrunnada <qilahqathxx@gmail.com>
2025-04-24 23:03:30 +0700

    Update StatusPelayananFragment

--- app/src/main/java/com/md/satuwargaapp/ui/pelayanan/StatusPelayananFragment.kt
+++ app/src/main/java/com/md/satuwargaapp/ui/pelayanan/StatusPelayananFragment.kt
@@ -5,14 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.md.satuwargaapp.R
 import com.md.satuwargaapp.databinding.FragmentStatusPelayananBinding
 
-class StatusPelayananFragment: Fragment() {
+class StatusPelayananFragment : Fragment(), OnStatusClickListener {
+
     private var _binding: FragmentStatusPelayananBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: StatusPelayananAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,14 +27,24 @@ class StatusPelayananFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView: RecyclerView = view.findViewById(R.id.rv_stat_layanan)
+        val mockData = listOf(
+            StatusPelayanan("Pelaporan Masalah", "Jalan rusak depan rumah", "Selasa, 01 April 2025", "Diproses"),
+            StatusPelayanan("Pengajuan Surat", "Surat keterangan domisili", "Rabu, 02 April 2025", "Selesai"),
+            StatusPelayanan("Pengajuan Validasi TTD RT RW", "Validasi Surat Keterangan Ubah KTP", "Jumat, 04 April 2025", "Selesai")
+        )
 
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        adapter = StatusPelayananAdapter(mockData, this)
+        binding.rvStatLayanan.adapter = adapter
+        binding.rvStatLayanan.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onStatusClick(status: StatusPelayanan) {
+        // Arahkan ke DetailStatusFragment
+        findNavController().navigate(R.id.action_statusPelayananFragment_toDetailStatusFragment)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
